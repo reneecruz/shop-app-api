550bd59c3b951bacc0253b26d41bfcd657321d26
Renée Cruz <reneebasscruz@gmail.com>
2019-11-22 17:54:36 -0500

    added custom route for /signup to users controller create path restful route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
 
   post "/login", to: "auth#login"
+  post "/signup", to: "users#create"
   get "/persist", to: "auth#persist"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
