b3c06ceb26dec748fe13fd876cb9130c946f4072
Renée Cruz <reneebasscruz@gmail.com>
2019-11-25 18:58:34 -0500

    added profile route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get "/persist", to: "auth#persist"
   post "/signup", to: "users#create"
 
+  get "/showpage", to: "items#show"
+  get "/account", to: "users#show"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
