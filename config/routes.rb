17515776f46a3ca2e247cb5b3b69959996d448bb
Renée Cruz <reneebasscruz@gmail.com>
2019-11-24 21:30:16 -0500

    added persist route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
 
   post "/login", to: "auth#login"
-  get "/auth", to: "auth#persist"
+  get "/persist", to: "auth#persist"
   post "/signup", to: "users#create"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
