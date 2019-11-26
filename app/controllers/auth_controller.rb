e2396dabd35a833a2849a52ec494c7fee1b08ccb
Renée Cruz <reneebasscruz@gmail.com>
2019-11-26 12:26:30 -0500

    trying to fix bcrypt invalid hash bug for seed data user

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,8 @@
 
     def login
         user = User.find_by(username: login_params[:username])
-        if user.valid? && user.authenticate(login_params[:password])
+        
+        if user && user.authenticate(login_params[:password])
             payload = {user_id: user.id} 
             token = JWT.encode(payload, secret, "HS256")
             render json: {user: user, token: token}
@@ -12,7 +13,7 @@
     end 
 
     def persist
-        encoded_token = request.headers['Authorization'].split(' ')[1]
+        encoded_token = request.headers['Authorization']
         decoded_token = JWT.decode(encoded_token, secret, true, { algorithm: 'HS256' })
         user_id = decoded_token[0]["user_id"]
         user = User.find(user_id)
