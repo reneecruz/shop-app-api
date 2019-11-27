0b8408e965e2c437bb596040bb89543c32491eb2
Renée Cruz <reneebasscruz@gmail.com>
2019-11-26 20:15:33 -0500

    create method for order_items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,5 +6,15 @@ class OrderItemsController < ApplicationController
 
     def show
     end
+
+    def create 
+        OrderItem.create(order_item_params)
+    end 
+
+    private 
+
+    def order_item_params 
+        params.permit(:order_id, :item_id)
+    end
     
 end
