0c541b05742b90c968ac59c669c9a793d4b4c46b
Renée Cruz <reneebasscruz@gmail.com>
2019-12-04 15:46:28 -0500

    total price method

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,11 @@ class Order < ApplicationRecord
 
     has_many :order_items
     has_many :items, through: :order_items
+
+    def total_price 
+        self.items.map(&:price).reduce(&:+)
+        #  self.items.reduce do |item| 
+        #     item.price 
+        #  end
+    end
 end
